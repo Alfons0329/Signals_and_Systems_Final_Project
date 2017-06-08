117d59df8d6208784191505f6dc51384699352a8
Alfons0329 <alfons.cs04@g2.nctu.edu.tw>
2017-06-08 21:14:08 +0900

    Added more untracked files and report

--- 1-b.py
+++ 1-b.py
@@ -11,12 +11,15 @@ for i in range(len(ts_data)):
     ts_data[i]=ts_data[i].split(',') #split each row with ',' token
 
 #initilaization
-extracted_final_data = np.append(ts_data[0][1:],ts_data[1][1:]) #concatenate altogether to match a 1d list and plot accodring to time
+extracted_final_data = np.append(ts_data[0][1:],ts_data[1][1:]) 
+#concatenate altogether to match a 1d list and plot accodring to time
 for i in range(2,len_of_tsdata):
-    extracted_final_data = np.append(extracted_final_data,ts_data[i][1:]) #concatenate altogether to match a 1d list
-#and plot accodring to time
+    extracted_final_data = np.append(extracted_final_data,ts_data[i][1:]) 
+	#concatenate altogether to match a 1d list and plot accodring to time
 extracted_final_data = extracted_final_data.astype(float) #change the datatype to floating point version
-time_axis = np.arange(0,183,0.001,float) #from 0 ro 182 seconds, the difference between each is 0.001 (as sampling freq = 1000 Hz)
+time_axis = np.arange(0,183,0.001,float) 
+#from 0 ro 182 seconds, the difference between each is 0.001 (as sampling freq = 1000 Hz)
 #plot as the floating type
-plt.plot(time_axis,np.absolute(extracted_final_data)) #using matlab api for drawing x axis as time_axis and y axis as np.absolute(extracted_final_data)
+plt.plot(time_axis,np.absolute(extracted_final_data)) 
+#using matlab api for drawing x axis as time_axis and y axis as np.absolute(extracted_final_data)
 plt.show()
